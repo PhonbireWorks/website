81dad3cae3f9acc6f4f0936531793540446ac835
PhonbireWorks <phonbireworks@gmail.com>
2024-10-15 16:21:21 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -2,3 +2,11 @@
 document.getElementById('hamburger').addEventListener('click', function() {
     document.getElementById('nav-menu').classList.toggle('active');
 });
+
+
+
+
+// script.js
+document.getElementById('scrollButton').addEventListener('click', function() {
+    document.getElementById('targetSection').scrollIntoView({ behavior: 'smooth' });
+});
