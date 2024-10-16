cd7a8d78441c37102bc77d9e4534d55d9b78b231
PhonbireWorks <phonbireworks@gmail.com>
2024-10-16 20:24:26 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -10,3 +10,32 @@ document.getElementById('hamburger').addEventListener('click', function() {
 document.getElementById('scrollButton').addEventListener('click', function() {
     document.getElementById('targetSection').scrollIntoView({ behavior: 'smooth' });
 });
+
+
+
+
+
+// script.js
+// Get the button
+let topBtn = document.getElementById("topBtn");
+
+// Show the button when the user scrolls down 20px from the top of the document
+window.onscroll = function() {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        topBtn.style.display = "block";
+    } else {
+        topBtn.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+}
+
+
